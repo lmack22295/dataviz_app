d87e73ade6de9246e303e94518eee10f0111e0a6
Lance  Mack <lancemack@Lances-MBP.fios-router.home>
2018-02-05 17:54:58 -0500

    add registration page and password hashing

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask_login import LoginManager
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -7,5 +8,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,12 +1,21 @@
 from app import db
+from app import login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     sites = db.relationship('Site', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -29,3 +38,7 @@ class Site(db.Model):
 
     def __repr__(self):
         return '<Site {}>'.format(self.description)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
